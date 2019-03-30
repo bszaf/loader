2e1c7b71c90ba942e81eaf34a668f8607d1d0262
Bartosz Szafran <bszafpl@gmail.com>
2019-03-30 11:13:42 +0100

    Add helper for retrieving process with the greatest queue

--- lib/loader.ex
+++ lib/loader.ex
@@ -2,4 +2,9 @@ defmodule Loader do
   @moduledoc """
   Documentation for Loader.
   """
+
+  def max_queue() do
+    [{_pid, count, _info}] = :recon.proc_count(:message_queue_len, 1)
+    count
+  end
 end
